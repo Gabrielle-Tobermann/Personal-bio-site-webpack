57fdaae6a0f6478defa65f117ba8dbce33ca8545
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-14 14:59:22 -0500

    created sample data, added firebase and api keys

--- src/javascripts/helpers/data/projectsData.js
+++ src/javascripts/helpers/data/projectsData.js
@@ -1,54 +1,12 @@
-const projects = [
-  {
-    title: 'PINTEREST',
-    screenshot: 'https://user-images.githubusercontent.com/76187279/111057355-8b09fa80-844c-11eb-9b04-d87ef533d7ab.png',
-    description: 'This is my version of Pinterest! It allows users to create boards and pins, which can be edited or deleted. The user can also use the search bar to find a board.',
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: 'https://gt-pinterest.netlify.app/', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/Gabrielle-Tobermann/ASSIGNMENT-Firebase-Pinterest',
-  },
-  {
-    title: 'TAMAGOTCHI',
-    screenshot: 'https://user-images.githubusercontent.com/76187279/109723189-5e640200-7b73-11eb-9d28-ed73715beecf.png',
-    description: "This app is a recreation of the 90's Tamagotchi. It allows users to take care of a digital pet. The pet can eat, sleep, fight or play. It is perfect for teaching children how to take care of a pet and responsibilities", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: ' https://gt-tamagotchi.netlify.app/', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/Gabrielle-Tobermann/ASSIGNMENT-Tamagotchi',
-  },
-  {
-    title: 'COOL PROJECT',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'COOL PROJECT',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'COOL PROJECT',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  }
-];
+import axios from 'axios';
+import firebaseConfig from '../apiKeys';
 
-export default projects;
+const dbUrl = firebaseConfig.databaseURL;
+
+const getProjects = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/projects.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+export default getProjects;
