ac28ba6889afed235473d9db221429a78c998efe
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-13 20:05:40 -0600

    can scroll to any part of the page from nav bar

--- src/javascripts/helpers/navEvents.js
+++ src/javascripts/helpers/navEvents.js
@@ -3,6 +3,21 @@ const navEvents = () => {
     const element = document.querySelector('#bioPage');
     element.scrollIntoView({ behavior: 'smooth' });
   });
+
+  document.querySelector('#navToTechnologies').addEventListener('click', () => {
+    const element = document.querySelector('#technologiesPage');
+    element.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  document.querySelector('#navToProjects').addEventListener('click', () => {
+    const element = document.querySelector('#projectsPage');
+    element.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  document.querySelector('#navToContact').addEventListener('click', () => {
+    const element = document.querySelector('#contactPage');
+    element.scrollIntoView({ behavior: 'smooth' });
+  });
 };
 
 export default navEvents;
